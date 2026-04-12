3b49ed8772c01c4d887f533c47b656a1305e9a03
SumitSahu444 <sumitkumarsahu141@gmail.com>
2026-04-12 19:22:53 +0530

    set port for deployement

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
